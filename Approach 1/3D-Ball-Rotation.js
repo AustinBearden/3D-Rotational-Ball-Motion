3369b9d96dad24f57d0a2c7f39503cfec967d99a
Austin Bearden <austinb99@4jlt.com>
2018-04-05 07:59:46 -0400

    Filling out Ball class

--- Approach 1/3D-Ball-Rotation.js
+++ Approach 1/3D-Ball-Rotation.js
@@ -3,3 +3,26 @@
 //File name: 3D-Ball-Rotation.js
 //Project: Project 2- Approach 1
 
+//creating the ball class
+//only argument is the ball's radius since this maters
+function Ball(radius) {
+
+    //initialize variables
+    this.Radius = radius;
+    this.X = 0.00;
+    this.Y = 0.00;
+    this.Z = 0.00;
+    
+
+
+    //declare and define methods
+    this.changeRadius = function(new_radius)
+    {
+        this.Radius = new_radius;
+
+    };
+
+
+
+
+}

979fce656bd29d7d9916d79f70cee230b2460a9d
Austin Bearden <austinb99@4jlt.com>
2018-04-07 21:07:28 -0400

    Added connectivity for input and output between HTML page and js

--- Approach 1/3D-Ball-Rotation.js
+++ Approach 1/3D-Ball-Rotation.js
@@ -42,6 +42,22 @@ function Ball(radius) {
     {
         this.Y = new_Y;
     };
+    
+    this.get_position_X = function()
+    {
+
+        //output to HTML page
+        document.getElementById("x-axis").innerHTML = "X-Position: " + this.X + " meters";
+
+    };
+
+    this.get_position_Y = function()
+    {
+
+        //output to HTML page
+        document.getElementById("y-axis").innerHTML = "Y-Position: " + this.Y + " meters";
+
+    };
 
     //movement method
     //Arguments: Angular velocity of the ball, direction based on angle from the positive X-axis,
@@ -54,8 +70,8 @@ function Ball(radius) {
         //1) change the angular velocity to linear velocity
         var lin_velcoity = this.Radius * ang_velocity;
         //2) split up the velocity into x and y components
-        var x_lin_velocity = lin_velcoity*Math.cos(direction_xy);
-        var y_lin_velocity = lin_velcoity*Math.sin(direction_xy);
+        var x_lin_velocity = lin_velcoity*Math.cos(direction_xy*Math.PI/180);
+        var y_lin_velocity = lin_velcoity*Math.sin(direction_xy*Math.PI/180);
         //find linear x and y placement of the ball
         var new_x = this.X + x_lin_velocity*time + 0.5*(this.ang_deceleration*this.Radius)*(Math.pow(time, 2));
         var new_y = this.Y + y_lin_velocity*time + 0.5*(this.ang_deceleration*this.Radius)*(Math.pow(time, 2));
@@ -66,6 +82,30 @@ function Ball(radius) {
         this.set_Y(new_y);
         
     };
+
+//execute calculations when button is clicked
+function calculate_all() {
+
+    //get values from input boxes
+    var radius_input = parseFloat(document.getElementById('radiusID').value);
+    console.log("Radius: " + radius_input);
+    var time_input = parseFloat(document.getElementById('timeID').value);
+    console.log("Time: " + time_input);
+    var ang_velocity_input = parseFloat(document.getElementById('ang_velocityID').value);
+    console.log("Angular Velocity: " + ang_velocity_input);
+    var XYdirection_input = parseFloat(document.getElementById('XYdirectionID').value);
+    console.log("XYdirection: " + XYdirection_input);
+
+    ball_move_1 = new Ball(radius_input);
+    ball_move_1.move(time_input, ang_velocity_input, XYdirection_input); //incomplete
+    ball_move_1.get_position_X();
+    ball_move_1.get_position_Y();
+    	
+}
+
+//Accesses button from html	
+var calculate = document.getElementById("calculate");
+calculate.addEventListener("click", function() { calculate_all(); } );
 
 
 
